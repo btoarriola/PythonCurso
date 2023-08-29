4ab7792690332875e019c7358187bff202de3b1a
btoarriola <106176468+btoarriola@users.noreply.github.com>
2023-08-29 09:07:11 -0600

    new python

---
+++ lesson1.py
@@ -0,0 +1,37 @@
+# Ejercicio 0
+color="blue"
+
+# Ejercicio 1
+pi = 3.14159 # approximate
+diameter = 3
+
+radius=diameter/2
+
+area=pi*(radius*radius)
+
+#Ejercicio 2
+
+
+a = [1, 2, 3]
+b = [3, 2, 1]
+q2.store_original_ids()
+
+c=a
+a=b
+b=c
+
+# Ejercicio 3a
+(5 - 3) // 2
+
+# Ejercicio 3b
+8 - 3 * 2 - (1 + 1)
+
+# Ejercicio 3b
+
+alice_candies = 121
+bob_candies = 77
+carol_candies = 109
+
+all_candies =(alice_candies + bob_candies + carol_candies)
+to_smash = (all_candies % 3)
+print ()
---
+++ lesson2.py
@@ -0,0 +1,15 @@
+# Ejercicio 1
+def round_to_two_places(num):
+
+    return round(num, 2)
+    pass
+round_to_two_places(3.14159)
+
+# Ejercicio 2
+print (round(3.141522229, -2))
+
+# Ejercicio 3
+def to_smash(total_candies, friends=3):
+    return total_candies % friends
+
+to_smash(91)
